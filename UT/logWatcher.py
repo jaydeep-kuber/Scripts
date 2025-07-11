f445377d29a5102525120f2c56fc2c165152f958
jaydeep kuber <jaydeep.kuber@brevitaz.com>
2025-07-11 13:07:48 +0530

    logwatcher with sns working fine

--- UT/logWatcher.py
+++ UT/logWatcher.py
@@ -6,9 +6,11 @@ import os
 import re
 import sys
 import time
+import queue
 import boto3
 import json
 import logging
+import threading
 from datetime import datetime
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
@@ -20,7 +22,7 @@ from logging.config import dictConfig
     Prevents re-reading the same lines over and over.
 """
 file_cursors = {}
-
+error_q = queue.Queue()
 # Extras
 def makefile():
     for file in LOG_FILES:        
@@ -41,15 +43,30 @@ def setup_logging():
     # apply logging configuration
     dictConfig(log_conf)
 
-def send_to_sqs(message):
-    try:
-        sqs.send_message(
-            QueueUrl=SQS_URL,
-            MessageBody=json.dumps({"log": message})
-        )
-        logger.info(f"Message sent to SQS: {message}")
-    except Exception as e:
-        logger.error(f"Failed to send message with: {e}")
+def send_to_sns(message):
+    pass
+
+def email_msg_maker(line, buffer, stillCollecting):
+    """ 
+        re => 2025-07-09 14:43:52 + "ERROR"
+        If line starts with timestamp AND contains "ERROR":
+            If we were already collecting:
+                - Finish previous error block - Push the block to queue - Clear buffer
+            Start a new error block:
+                - Add current line to buffer - Set collecting = True
+
+        Else if line starts with timestamp (but no "ERROR"):
+            If we were collecting:
+                - Finish current error block - Push block to queue - Clear buffer - Set collecting = False
+            Else:
+                - Ignore the line
+
+        Else (line has no timestamp):
+            If collecting:
+                - This line is part of current traceback - Append to buffer
+
+        Return updated buffer and collecting flag 
+    """
 
 def read_new_lines(filepath):
     """ 
@@ -57,6 +74,7 @@ def read_new_lines(filepath):
         We set cursor to end of file (like tail -f).
         This avoids reading logs that already happened.
     """
+    logger.info(f"Reading new lines from: {filepath}")
     if filepath not in file_cursors:
         file_cursors[filepath] = os.path.getsize(filepath) # rtn file size in bytes 10KB → returns 10240
         return  # first run, skip existing lines
@@ -72,9 +90,14 @@ def read_new_lines(filepath):
         file_cursors[filepath] = f.tell() # returns the current position in bytes
 
     for line in new_lines:
-        if LOG_PATTERN.search(line):
-            send_to_sqs(line.strip())
-
+        if TIMESTAMP_RE.search(line): # re for timestamp
+            if LOG_PATTERN.search(line):
+                sns.publish(
+                TopicArn=TOPIC_ARN,
+                Subject="🚨 Error Digest",
+                Message=line.strip()
+                )
+            
 # --- Watchdog Handler ---
 class LogChangeHandler(FileSystemEventHandler):
     def on_modified(self, event):
@@ -112,23 +135,40 @@ def config_loader(path):
     with open(path, 'r') as f:
         configs = json.load(f)
     
-    global REGION, SQS_URL, LOG_FILES
+    global REGION, TOPIC_ARN, LOG_FILES
     REGION = configs['REGION']
-    SQS_URL = configs['SQS_URL']
+    TOPIC_ARN = configs['TOPIC_ARN']
     LOG_FILES = configs['LOG_FILES']
 
     return True
 
 if __name__ == "__main__":
+    # setting up logging
     setup_logging()
-    LOG_PATTERN = re.compile(r'error|critical|fatal|GET', re.IGNORECASE)
     logger = logging.getLogger("script")
+
+    # setting up timer to prevent mail overhead
+    COOLDOWN = 60  # in seconds
+    last_sent_time = 0 # in seconds
+    current_time = time.time()
+
+    # define pattern using re. 
+    # LOG_PATTERN = re.compile(r'error|critical|fatal', re.IGNORECASE)
+    LOG_PATTERN = re.compile(r'error|exception|traceback|fatal|fail(ed)?|panic|crash|unhandled', re.IGNORECASE)
+
+    TIMESTAMP_ERROR_RE = re.compile(r'^\d{4}/\d{2}/\d{2} \d{2}:\d{2}:\d{2}.*ERROR', re.IGNORECASE)
+    TIMESTAMP_RE = re.compile(r'^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d{3}')
+
+    # check if config file is provided
     config_path = sys.argv[1] if len(sys.argv) > 1 else sys.exit("Please provice a config.json in first arg...")
     logger.info(f"Using configuring from: {config_path} file")
 
+    # Error Queue
     if config_loader(config_path):
-    # sqs client
-        sqs = boto3.client('sqs', region_name=REGION)    
+        # sqs client
+        # sqs = boto3.client('sqs', region_name=REGION)    
+        sns = boto3.client('sns', region_name=REGION)
+        logger.error("Failed to create SNS client...") if not sns else logger.info("SNS client created successfully.")
         start_monitoring()
     else:
         logger.error("Failed to load configuration.")
