5002cafbd3674448f8f8b789bcf11f36248b9261
jaydeep kuber <jaydeep.kuber@brevitaz.com>
2025-07-31 11:19:39 +0530

    removed unwanted directories

--- others/scholarHealthCheck.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# check is all containers are runninig from given list.
-# if any ofthen is not running  then (do something, but what to do? )
-# -- try to find it in [docker ps -a]. if we found it then simple do restart,
-# -- check resart status, ? is it success or failed
-# -- if failed then we can do remove this container and run respetive docker command. 
